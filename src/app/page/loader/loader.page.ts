5d7c5d5709d64105dd3704ae0e3ff072bd3891f9
arcaneonLap <arcanoleon@gmail.com>
2022-09-01 11:57:13 -0500

    prev to deleting folder

--- src/app/page/loader/loader.page.ts
+++ src/app/page/loader/loader.page.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+/* Este selector nos permite "llamar" (mostrar, no linkear) esta página desde otros lados de la app 
+  * sin embargo, si invocamos la página a través de este método, necesitamos declararla en el módulo 
+  * de la página donde queramos 
+*/
+
 @Component({
   selector: 'app-loader',
   templateUrl: './loader.page.html',
@@ -8,6 +13,10 @@ import { Component, OnInit } from '@angular/core';
 export class LoaderPage implements OnInit {
 
   constructor() { }
+
+/* ngOnInit -> "angular On Initiation"
+  * es una función que se ejecuta al inicializar la página
+*/
 
   ngOnInit() {
   }
