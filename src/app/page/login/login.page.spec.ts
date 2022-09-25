d059746d868443559938302479fb94f6351df899
arcaneonLap <arcanoleon@gmail.com>
2022-09-25 11:40:27 -0500

    ok

--- src/app/page/login/login.page.spec.ts
+++ src/app/page/login/login.page.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 
 import { LoginPage } from './login.page';
@@ -6,6 +7,7 @@ import { LoginPage } from './login.page';
 describe('LoginPage', () => {
   let component: LoginPage;
   let fixture: ComponentFixture<LoginPage>;
+  let router: Router;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
@@ -14,6 +16,8 @@ describe('LoginPage', () => {
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginPage);
+    router = TestBed.get(Router);
+
     component = fixture.componentInstance;
     fixture.detectChanges();
   }));
@@ -21,4 +25,5 @@ describe('LoginPage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
